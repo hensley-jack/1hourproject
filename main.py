c89b9921f4b1c5ce4ac8387d49877ccffd4f1eae
hensley-jack <jh652123@ohio.edu>
2025-11-13 17:45:55 -0500

    Hi

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from google import genai
+from google.genai import types
+import requests
+
+GEMINI_API_KEY="AIzaSyBsqItdLobUoqGbydFb0IQTyTZvni4vgqY"
+
+
+statement = "Please analyze wether the following statement is true or false. Think wisely and deeply. Your response should ONLY be a 'T' or an 'F'. "
+statement = statement + input("Enter your statement: ")
+
+client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
+config = types.GenerateContentConfig()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="statement",
+    config=config,
+)
+
+# Print the final, user-facing response
+print(response.text)
